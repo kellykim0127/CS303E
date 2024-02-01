af1faa40cfd9df4f861eb0feb344b6530d0766bf
kellykim0127 <147536434+kellykim0127@users.noreply.github.com>
2024-02-01 15:06:47 -0600

    Add files via upload

---
+++ HW1A.py
@@ -0,0 +1,26 @@
+print("Hello, World!")
+print()
+
+print("UUU   " + "UUU   " + "TTTTTTTTT")
+print(" U     " + "U    " + "T   " + "T   " + "T")
+print(" U     " + "U        " + "T")
+print(" U     " + "U        " + "T")
+print(" U     " + "U        " + "T")
+print("  UUUUU        " + "TTT")
+print()
+
+print("   /  " + "\\")
+print("  /  " + "/ " + "\\")
+print(" /  " + "/   " + "\\")
+print(" \\  " "\\   " + "/")
+print("  \\ _" + "\\_" + "/")
+print("     \\" + "\\     "  "_ " +"_")
+print("      \\" + "\\  " + "/   " + "/")
+print("       \\" + "\\" + "/" + "_ _" + "/")
+print("       //")
+print()
+
+print("One of my favorite songs is \"Cruel Summer\" by Taylor Swift.")
+print()
+
+print("I would like to visit:\nParis, France!")
